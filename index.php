3515a509ed2a63d7f8256d693ad8d5ae0b4bd0c0
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-02-28 17:45:28 +0000

    init

--- index.php
+++ index.php
@@ -2,41 +2,20 @@
 <!-- ICS2O-Unit1-07-HTML-MDL-PHP -->
 <html lang="en-ca">
   <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="Hello, World!, PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-    <link
-      rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
-    />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="./images/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="./images/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="./images/favicon-16x16.png"
-    />
-    <link rel="manifest" href="./site.webmanifest" />
+  <meta charset="utf-8" />
+  <meta name="description" content="My first webpage, with JS" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Adrina.peighambarzadeh" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/android-chrome-192x192.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
     <title>Hello, World!, PHP</title>
   </head>
-
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
