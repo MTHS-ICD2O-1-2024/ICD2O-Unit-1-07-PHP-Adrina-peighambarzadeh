13ada81fa666dfd5761d2abc337799a42a193fb0
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-02-28 18:28:44 +0000

    changed code

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="192x192" href="./images/android-chrome-192x192.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png" />
-    <link rel="manifest" href="site.webmanifest" />
+    <link rel="manifest" href="./images/site.webmanifest" />
     <title>Hello, World!, PHP</title>
   </head>
   <body>
